a155d1b8b3bed3e603c0a42b117de7b7c0d180e3
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2023-11-28 19:53:22 -0500

    add pass

--- pssec/webapp/views.py
+++ pssec/webapp/views.py
@@ -6,10 +6,10 @@ import sqlite3
 def buscar_usuario(nombre_usuario):
     conexion = sqlite3.connect("base_de_datos.db")
     cursor = conexion.cursor()
-
+    password = 'abcd123'
     # ¡Vulnerabilidad! No se realiza la sanitización de la entrada del usuario.
     cursor.execute("SELECT * FROM usuarios WHERE nombre = '" + nombre_usuario + "'")
-
+    print(password)
     usuario = cursor.fetchone()
 
     conexion.close()
